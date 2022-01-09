031806dbe4320cc27c11aa97c075989b443a5779
Hammad <hammad2091504@hotmail.com>
2022-01-09 17:08:09 +0000

    Collect sales data from last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -106,6 +106,21 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entires_sales():
+    """
+    docstring
+    """
+    sales = SHEET.worksheet("sales")
+    # column = sales.col_values(3)
+    # print(column)
+    
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 
 def main():
     """ 
@@ -119,4 +134,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_colums = get_last_5_entires_sales()
